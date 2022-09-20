3d59209bda44ad659b48a077939fc7615c9ebf20
Santiago Di Deo <santiagodideo@gmail.com>
2022-09-20 20:45:08 -0300

    PreEntrega1+DiDeo

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -5,7 +5,7 @@ const ItemListContainer = ({greeting}) => {
 
     return (
         <div className='tituloSaludo'>
-            <h1 className="productos" style={{textAlign:'center'}}>Productos</h1>
+            <h1 className="productos">Productos</h1>
             <h3 className="greeting">{greeting}</h3>
         </div>
     );
