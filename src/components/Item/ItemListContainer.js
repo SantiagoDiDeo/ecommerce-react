e7e0504b6f37da5f1c5ece2c2a28cbcf85a4c81e
Santiago Di Deo <santiagodideo@gmail.com>
2022-10-05 21:54:01 -0300

    PreEntrega2+DiDeo

--- src/components/Item/ItemListContainer.js
+++ src/components/Item/ItemListContainer.js
@@ -11,7 +11,7 @@ const ItemListContainer = ({greeting}) => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        console.log(categoryId)
+    
     },[categoryId]);
 
     useEffect(() => {
